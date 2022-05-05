40fdb61062bea1eda7a57ff95fd4c5e1d0b49490
James Luty <luty0125@gmail.com>
2022-05-04 18:11:22 -0700

    initial upload of react blocks

---
+++ src/Components/SubContents.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import styles from './styles.module.css'
+
+class SubContents extends Component {
+    render() {
+        return (
+            <div className={styles.sub_contents}></div>
+        )
+    }
+}
+
+export default SubContents
